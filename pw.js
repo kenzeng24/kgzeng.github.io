7a45552a4a5f42f1a8241e50e79fa2a358fafef5
kenzeng24 <kgz1@rice.edu>
2017-09-24 01:00:09 -0500

    Added index.html file

---
+++ pw.js
@@ -0,0 +1,13 @@
+function new_event() {
+    
+    var event = document.getElementById("event").value;
+    var day = document.getElementById("day").value;
+    var time = document.getElementById("time").value
+    var dayTime = day.concat(time)
+    
+    document.getElementById(dayTime).innerHTML = event;
+}
+
+
+
+
